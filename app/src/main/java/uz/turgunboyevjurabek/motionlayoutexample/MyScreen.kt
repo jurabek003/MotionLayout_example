0265e2b31e2205eb1fcffef66de10acf8a63ea30
Asus <turgunboyevjurabek777@gmail.com>
2024-09-21 11:07:18 +0500

    simple

--- app/src/main/java/uz/turgunboyevjurabek/motionlayoutexample/MyScreen.kt
+++ app/src/main/java/uz/turgunboyevjurabek/motionlayoutexample/MyScreen.kt
@@ -1,4 +1,6 @@
-@file:OptIn(ExperimentalFoundationApi::class, ExperimentalMotionApi::class)
+@file:OptIn(ExperimentalFoundationApi::class, ExperimentalMotionApi::class,
+    ExperimentalMaterial3Api::class
+)
 
 package uz.turgunboyevjurabek.motionlayoutexample
 
@@ -18,11 +20,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,6 +39,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -80,16 +87,17 @@ fun MyScreen(modifier: Modifier) {
             .fillMaxWidth()
     ) {
         val properties = motionProperties(id = "profile_pic")
+
         Box(
             modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .layoutId("box")
                 .anchoredDraggable(
                     state = anchoredDraggableState,
                     orientation = Orientation.Vertical,
                     reverseDirection = true // Yo'nalishni invers qildik
                 )
-                .fillMaxWidth()
-                .background(Color.DarkGray)
-                .layoutId("box")
         )
         Image(
             painter = painterResource(id = R.drawable.jetpack_compose),
@@ -112,11 +120,11 @@ fun MyScreen(modifier: Modifier) {
         )
         Column(
             modifier=Modifier
+                .fillMaxWidth()
                 .layoutId("actions"),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-//            Spacer(modifier = Modifier.height(20.dp))
             LazyColumn {
                items(50){
                    ListUI()
